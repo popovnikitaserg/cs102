b2e20379cbfbf9d99847cc1d9317e6aa9419de22
popovnikitaserg <nikita@DESKTOP-ITJC9NP.localdomain>
2023-11-11 20:59:56 +0300

    Исправления

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -147,6 +147,7 @@ def solve(grid: tp.List[tp.List[str]]) -> list[list[str]] | None:
                     grid[row][col] = "."
     return
 
+
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """Если решение solution верно, то вернуть True, в противном случае False"""
     # TODO: Add doctests with bad puzzles
