ad129d371a769ca1d812798e27f592a19b4dda88
popovnikitaserg <nikita@DESKTOP-ITJC9NP.localdomain>
2023-11-11 18:44:28 +0300

    Функция get_col()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -65,7 +65,7 @@ def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    pass
+    return [tuple(i for i in grid[j][pos[1]])[0] for j in range(len(grid))]
 
 
 def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
