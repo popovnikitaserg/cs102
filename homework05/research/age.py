89777bcd7e000a384b55afb12a12d78f7727cea0
popovnikitaserg <nikita@DESKTOP-ITJC9NP.localdomain>
2024-05-03 23:55:17 +0300

    age.py written

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -14,4 +14,17 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+    friends = get_friends(user_id=user_id, fields=["bdate"])
+    print(friends)
+    years = []
+    for i in friends.items:
+        try:
+            years.append(i['bdate'])
+        except:
+            pass
+    years = [i[-4:] for i in years]
+    years = [i.replace(".", "") for i in years]
+    years = [i for i in years if len(i) == 4]
+    ages = [2024 - int(i) for i in years]
+    ages = sorted(ages)
+    return ages[len(ages) // 2] if len(ages) > 0 else None
