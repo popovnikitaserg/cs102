9e1f198f092dc0d8ee4b45b7b5f4c47dfa63022f
popovnikitaserg <nikita@DESKTOP-ITJC9NP.localdomain>
2024-05-08 14:04:07 +0300

    mypy hw5

--- homework05/tests/tests_api/test_session.py
+++ homework05/tests/tests_api/test_session.py
@@ -3,8 +3,12 @@ import unittest
 
 import httpretty
 import responses  # type: ignore
-from requests.exceptions import (ConnectionError, HTTPError,  # type: ignore
-                                 ReadTimeout, RetryError)
+from requests.exceptions import (  # type: ignore
+    ConnectionError,
+    HTTPError,
+    ReadTimeout,
+    RetryError,
+)
 from vkapi.session import Session
 
 
