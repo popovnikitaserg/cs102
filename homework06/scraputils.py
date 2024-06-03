c80254e144976a3fd7a4bbd9880e6be1ec66f7da
popovnikitaserg <nikita@DESKTOP-ITJC9NP.localdomain>
2024-06-03 11:28:59 +0500

    Scraputils

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -5,15 +5,24 @@ from bs4 import BeautifulSoup
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
-
-    # PUT YOUR CODE HERE
-
+    news = parser.findAll("tr", class_="athing")
+    for i in range(len(news)):
+        item = {}
+        found1 = news[i].findAll("a")[1]
+        item["url"] = found1["href"]
+        item["title"] = found1.string
+        sibl = news[i].next_sibling.findAll("a")
+        item["comments"] = int(sibl[4].string[0]) if sibl[4].string != "discuss" else 0
+        item["author"] = sibl[0].string
+        item["points"] = int(news[i].next_sibling.find("span", class_="score").string[0])
+        news_list.append(item)
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    next = parser.findAll("a", class_="morelink")
+    return next[0]["href"]
 
 
 def get_news(url, n_pages=1):
@@ -29,4 +38,3 @@ def get_news(url, n_pages=1):
         news.extend(news_list)
         n_pages -= 1
     return news
-
