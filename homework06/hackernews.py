595d7332d5b8e397382cd269663c185e70531869
popovnikitaserg <nikita@DESKTOP-ITJC9NP.localdomain>
2024-06-03 11:29:41 +0500

    hackernews added

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,8 +1,8 @@
 from bottle import (
     route, run, template, request, redirect
 )
-
-from scrapper import get_news
+import string
+from scraputils import get_news
 from db import News, session
 from bayes import NaiveBayesClassifier
 
@@ -16,21 +16,69 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    label = request.query.label
+    id_ = request.query.id
+    s = session()
+    news = s.query(News).get(id_)
+    news.label = label
+    s.commit()
     redirect("/news")
+
+
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator)
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    new_news = get_news("https://news.ycombinator.com/newest", 1)
+    s = session()
+    for i in new_news:
+        title = i["title"]
+        author = i["author"]
+        exists = s.query(News).filter(News.title == title and News.author == author).first()
+        if exists:
+            continue
+        news = News(**i)
+        s.add(news)
+        s.commit()
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    model = NaiveBayesClassifier()
+    s = session()
+    X_train = []
+    y_train = []
+    cl_news = s.query(News).filter(News.label != None).all()
+    for news in cl_news:
+        X_train.append(clean(news.title).lower())
+        y_train.append(news.label)
+    model.fit(X_train, y_train)
+    X = []
+    notcl_news = s.query(News).filter(News.label == None).all()
+    for news in notcl_news:
+        X.append(clean(news.title).lower())
+    predicts = model.predict(X)
+    for label, news in zip(predicts, notcl_news):
+        news.label = label
+    s.commit()
+    ans = []
+    classes = ["good", "maybe", "never"]
+    for cl in classes:
+        for news in notcl_news:
+            if news.label == cl:
+                ans.append(news)
+    return ans
+
+
+@route('/recommendations')
+def recommendations():
+    classified_news = classify_news()
+    return template('recs_template', rows=classified_news)
 
 
 if __name__ == "__main__":
     run(host="localhost", port=8080)
-
